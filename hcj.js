4c65e45300bb797467605fd2ddf2028ecb6b3b03
Jefferson Carpenter <jeffersoncarpenter2@gmail.com>
2015-08-07 00:12:17 -0500

    remove type checking

--- hcj.js
+++ hcj.js
@@ -127,99 +127,97 @@ var wireChildren = function (f) {
 };
 
 // add some syntactic sugar for calling and and all
-var component = hasType(
-	func([func([Context], Instance)], Component),
-	function (build) {
-		var comp = {
-			create: function (context) {
-				var instance = this.build(context);
+var component = function (build) {
+	var comp = {
+		create: function (context) {
+			var instance = this.build(context);
 
-				instance.wireChildren = instance.wireChildren || function () {};
+			instance.wireChildren = instance.wireChildren || function () {};
 
-				Q.all(instance.childComponentPs).then(function (childComponents) {
-					var childContexts = [];
-					var childInstances = childComponents.map(function (childComponent) {
-						if ($.isArray(childComponent)) {
-							var ctxs = [];
-							var is = childComponent.map(function (c) {
-								var ctx = instance.newCtx();
-								ctxs.push(ctx);
-								return c.create(ctx);
-							});
-							childContexts.push(ctxs);
-							return is;
-						}
-						else {
+			Q.all(instance.childComponentPs).then(function (childComponents) {
+				var childContexts = [];
+				var childInstances = childComponents.map(function (childComponent) {
+					if ($.isArray(childComponent)) {
+						var ctxs = [];
+						var is = childComponent.map(function (c) {
 							var ctx = instance.newCtx();
-							childContexts.push(ctx);
-							return childComponent.create(ctx);
-						}
-					});
-					
-					var resultContexts = instance.wireChildren.apply(null, [instance, context].concat(childInstances)) || [];
-					
-					for (var i = 0; i < childInstances.length; i++) {
-						var resultContext = resultContexts[i] || {};
-						
-						var childInstance = childInstances[i];
-						var childContext = childContexts[i];
-						
-						var applyResult = function (resultContext, childInstance, childContext) {
-							resultContext = resultContext || {};
-							(resultContext.top || Stream.once(0)).pushAll(childContext.top);
-							(resultContext.left || Stream.once(0)).pushAll(childContext.left);
-							(resultContext.width || childInstance.minWidth).pushAll(childContext.width);
-							(resultContext.height || childInstance.minHeight).pushAll(childContext.height);
-							(resultContext.backgroundColor || Stream.never()).pushAll(childContext.backgroundColor);
-							(resultContext.fontColor || Stream.never()).pushAll(childContext.fontColor);
-						};
-
-						if ($.isArray(childInstance)) {
-							for (var j = 0; j < childInstance.length; j++) {
-								applyResult(resultContext[j], childInstance[j], childContext[j]);
-							}
-						}
-						else {
-							applyResult(resultContext, childInstance, childContext);
-						}
+							ctxs.push(ctx);
+							return c.create(ctx);
+						});
+						childContexts.push(ctxs);
+						return is;
+					}
+					else {
+						var ctx = instance.newCtx();
+						childContexts.push(ctx);
+						return childComponent.create(ctx);
 					}
 				});
 				
-				return instance;
-			},
-			build: build,
-			and: function (f) {
-				var that = this;
-				var c = $.extend({}, that);
-				c.build = function (context) {
-					var i = that.build(context);
-					var destroyF = f(i, context);
-					var destroy = i.destroy;
-					if (destroyF) {
-						i.destroy = function () {
-							var p = destroyF();
-							if (p) {
-								p.then(function () {
-									destroy();
-								});
-							}
-							else {
-								destroy();
-							}
-						};
+				var resultContexts = instance.wireChildren.apply(null, [instance, context].concat(childInstances)) || [];
+				
+				for (var i = 0; i < childInstances.length; i++) {
+					var resultContext = resultContexts[i] || {};
+					
+					var childInstance = childInstances[i];
+					var childContext = childContexts[i];
+					
+					var applyResult = function (resultContext, childInstance, childContext) {
+						resultContext = resultContext || {};
+						(resultContext.top || Stream.once(0)).pushAll(childContext.top);
+						(resultContext.left || Stream.once(0)).pushAll(childContext.left);
+						(resultContext.width || childInstance.minWidth).pushAll(childContext.width);
+						(resultContext.height || childInstance.minHeight).pushAll(childContext.height);
+						(resultContext.backgroundColor || Stream.never()).pushAll(childContext.backgroundColor);
+						(resultContext.fontColor || Stream.never()).pushAll(childContext.fontColor);
+					};
+
+					if ($.isArray(childInstance)) {
+						for (var j = 0; j < childInstance.length; j++) {
+							applyResult(resultContext[j], childInstance[j], childContext[j]);
+						}
 					}
-					return i;
-				};
-				return c;
-			},
-			all: function (fs) {
-				return fs.reduce(function (c, f) {
-					return c.and(f);
-				}, this);
-			},
-		};
-		return comp;
-	});
+					else {
+						applyResult(resultContext, childInstance, childContext);
+					}
+				}
+			});
+			
+			return instance;
+		},
+		build: build,
+		and: function (f) {
+			var that = this;
+			var c = $.extend({}, that);
+			c.build = function (context) {
+				var i = that.build(context);
+				var destroyF = f(i, context);
+				var destroy = i.destroy;
+				if (destroyF) {
+					i.destroy = function () {
+						var p = destroyF();
+						if (p) {
+							p.then(function () {
+								destroy();
+							});
+						}
+						else {
+							destroy();
+						}
+					};
+				}
+				return i;
+			};
+			return c;
+		},
+		all: function (fs) {
+			return fs.reduce(function (c, f) {
+				return c.and(f);
+			}, this);
+		},
+	};
+	return comp;
+};
 
 var findMinWidth = function ($el) {
 	var $sandbox = $('.sandbox');
@@ -248,92 +246,90 @@ var findMinHeight = function ($el) {
 	return height;
 };
 
-var el = hasType(
-	func(string, Component),
-	function (name) {
-		return component(function (context) {
-			var $el = $(document.createElement(name));
-			context.$el.append($el);
+var el = function (name) {
+	return component(function (context) {
+		var $el = $(document.createElement(name));
+		context.$el.append($el);
 
-			context.top.map(function (t) {
-				updateWindowWidth();
-				$el.css('top', px(t));
-			});
-			context.left.map(function (l) {
-				$el.css('left', px(l));
-			});
-			context.width.map(function (w) {
-				$el.css('width', px(w));
-			});
-			context.height.map(function (h) {
-				updateWindowWidth();
-				$el.css('height', px(h));
-			});
-			context.backgroundColor.map(function (bgcolor) {
-				$el.css('background-color', bgcolor);
-			});
-			context.fontColor.map(function (bgcolor) {
-				$el.css('color', bgcolor);
-			});
+		context.top.map(function (t) {
+			updateWindowWidth();
+			$el.css('top', px(t));
+		});
+		context.left.map(function (l) {
+			$el.css('left', px(l));
+		});
+		context.width.map(function (w) {
+			$el.css('width', px(w));
+		});
+		context.height.map(function (h) {
+			updateWindowWidth();
+			$el.css('height', px(h));
+		});
+		context.backgroundColor.map(function (bgcolor) {
+			$el.css('background-color', bgcolor);
+		});
+		context.fontColor.map(function (bgcolor) {
+			$el.css('color', bgcolor);
+		});
 
-			var minWidth = Stream.once(0);
-			var minHeight = Stream.once(0);
+		var minWidth = Stream.once(0);
+		var minHeight = Stream.once(0);
 
-			var childComponentPs = [];
-			
-			var scrollStream = Stream.combine([context.scroll, context.top], function (scroll, top) {
-				return scroll - top;
-			});
-			var newCtx = function () {
-				return {
-					$el: $el,
-					scroll: scrollStream,
-					top: Stream.once(0),
-					left: Stream.once(0),
-					width: Stream.never(),
-					height: Stream.never(),
-					backgroundColor: Stream.never(),
-					fontColor: Stream.never(),
-				};
-			};
-			
+		var childComponentPs = [];
+		
+		var scrollStream = Stream.combine([context.scroll, context.top], function (scroll, top) {
+			return scroll - top;
+		});
+		var newCtx = function () {
 			return {
 				$el: $el,
-				optimalWidth: 0,
-				minWidth: minWidth,
-				minHeight: minHeight,
-				newCtx: newCtx,
-				childComponentPs: childComponentPs,
-				child: function (component) {
-					childComponentPs.push(component);
-				},
-				children: function (components) {
-					childComponentPs.push(components);
-				},
-				destroy: function () {
-					minWidth.push(0);
-					minHeight.push(0);
-
-					Q.all(allInstancePs).then(function (allInstances) {
-						for (var i = 0; i < allInstances.length; i++) {
-							var instance = allInstances[i];
-							if ($.isArray(instance)) {
-								var instances = instance;
-								instances.map(function (i) {
-									i.destroy();
-								});
-							}
-							else {
-								instance.destroy();
-							}
-						}
-						
-						$el.remove();
-					});
-				},
+				scroll: scrollStream,
+				top: Stream.once(0),
+				left: Stream.once(0),
+				width: Stream.never(),
+				height: Stream.never(),
+				backgroundColor: Stream.never(),
+				fontColor: Stream.never(),
 			};
-		});
+		};
+		
+		return {
+			$el: $el,
+			optimalWidth: 0,
+			minWidth: minWidth,
+			minHeight: minHeight,
+			newCtx: newCtx,
+			childComponentPs: childComponentPs,
+			child: function (component) {
+				childComponentPs.push(component);
+			},
+			children: function (components) {
+				childComponentPs.push(components);
+			},
+			destroy: function () {
+				minWidth.push(0);
+				minHeight.push(0);
+
+				Q.all(allInstancePs).then(function (allInstances) {
+					for (var i = 0; i < allInstances.length; i++) {
+						var instance = allInstances[i];
+						if ($.isArray(instance)) {
+							var instances = instance;
+							instances.map(function (i) {
+								i.destroy();
+							});
+						}
+						else {
+							instance.destroy();
+						}
+					}
+					
+					$el.remove();
+				});
+			},
+		};
 	});
+};
 
 
 // div :: Component
@@ -346,7 +342,7 @@ var textarea = el('textarea');
 var ul = el('ul');
 
 var rootContext = function () {
-	return hasType(Context, {
+	return {
 		$el: $('body'),
 		top: Stream.once(0),
 		left: Stream.once(0),
@@ -355,7 +351,7 @@ var rootContext = function () {
 		height: Stream.never(),
 		backgroundColor: Stream.never(),
 		fontColor: Stream.never(),
-	});
+	};
 };
 
 var rootComponent = function (component) {
--- libs.js
+++ libs.js
@@ -1,16 +1,15 @@
 var Unit = func(Number, String);
-var unit = hasType(
-	func(String),
-	function (unit) {
-		return hasType(Unit, function (number) {
-			return number + unit;
-		});
-	});
+var unit = function (unit) {
+	return function (number) {
+		return number + unit;
+	};
+};
 var px = unit('px');
 
 var url = function (str) {
 	return 'url("' + str + '")';
 };
+
 
 
 // $$ :: String -> [*] -> Component -> Component
@@ -24,33 +23,23 @@ var $$ = function (func) {
 	};
 };
 
-var $addClass = hasType(
-	func(String),
-	$$('addClass'));
+var $addClass = $$('addClass');
 
-var $css = hasType(
-	func([String, String]),
-	$$('css'));
+var $css = $$('css');
 
-var $attr = hasType(
-	func([String, String]),
-	$$('attr'));
+var $attr = $$('attr');
 
-var $prop = hasType(
-	func([String, String]),
-	$$('prop'));
+var $prop = $$('prop');
 
-var $html = hasType(
-	func(String),
-	function (string) {
-		return function (i) {
-			i.$el.html(string);
-			var mw = findMinWidth(i.$el);
-			var mh = findMinHeight(i.$el);
-			i.minWidth.push(mw);
-			i.minHeight.push(mh);
-		};
-	});
+var $html = function (string) {
+	return function (i) {
+		i.$el.html(string);
+		var mw = findMinWidth(i.$el);
+		var mh = findMinHeight(i.$el);
+		i.minWidth.push(mw);
+		i.minHeight.push(mh);
+	};
+};
 
 
 var windowWidth = Stream.never();
@@ -187,51 +176,49 @@ var sideBySide = function (cs) {
 	]);
 };
 
-var stack = hasType(
-	func([list(Component)], Component),
-	function (cs) {
-		return div.all([
-			componentName('stack'),
-			children(cs),
-			wireChildren(function (instance, context, is) {
-				var totalMinHeightStream = function (is) {
-					return Stream.combine(is.map(function (i) {
-						return i.minHeight;
-					}), function () {
-						var args = Array.prototype.slice.call(arguments);
-						return args.reduce(function (a, b) {
-							return a + b;
-						}, 0);
-					});
-				};
-
-				var contexts = [];
-				is.reduce(function (is, i) {
-					var tops = totalMinHeightStream(is);
-					contexts.push({
-						top: tops,
-						width: context.width,
-						height: i.minHeight,
-					});
-
-					is.push(i);
-					return is;
-				}, []);
-
-				totalMinHeightStream(is).pushAll(instance.minHeight);
-				Stream.combine(is.map(function (i) {
-					return i.minWidth;
+var stack = function (cs) {
+	return div.all([
+		componentName('stack'),
+		children(cs),
+		wireChildren(function (instance, context, is) {
+			var totalMinHeightStream = function (is) {
+				return Stream.combine(is.map(function (i) {
+					return i.minHeight;
 				}), function () {
 					var args = Array.prototype.slice.call(arguments);
 					return args.reduce(function (a, b) {
-						return Math.max(a, b);
+						return a + b;
 					}, 0);
-				}).pushAll(instance.minWidth);
-				
-				return [contexts];
-			}),
-		]);
-	});
+				});
+			};
+
+			var contexts = [];
+			is.reduce(function (is, i) {
+				var tops = totalMinHeightStream(is);
+				contexts.push({
+					top: tops,
+					width: context.width,
+					height: i.minHeight,
+				});
+
+				is.push(i);
+				return is;
+			}, []);
+
+			totalMinHeightStream(is).pushAll(instance.minHeight);
+			Stream.combine(is.map(function (i) {
+				return i.minWidth;
+			}), function () {
+				var args = Array.prototype.slice.call(arguments);
+				return args.reduce(function (a, b) {
+					return Math.max(a, b);
+				}, 0);
+			}).pushAll(instance.minWidth);
+			
+			return [contexts];
+		}),
+	]);
+};
 
 var padding = function (amount, c) {
 	var top = 0, bottom = 0, left = 0, right = 0;
@@ -502,117 +489,115 @@ var GridConfig = object({
 });
 
 
-var grid = hasType(
-	func(GridConfig, list(Component)),
-	function (config, cs) {
-		config.gutterSize = config.gutterSize || 0;
-		config.minColumnWidth = config.minColumnWidth || 100;
-		return padding(config.outerGutter ? config.gutterSize : 0, div.all([
-			children(cs.map(function (c) {
-				return c.all([
-					$css('transition', 'top 0.5s, left 0.5s, width 0.5s'),
-				]);
-			})),
-			wireChildren(function (instance, context, is) {
-				var gridCellCount = context.width.map(function (width) {
-					return Math.floor(width / config.minColumnWidth / 2) * 2;
-				});
+var grid = function (config, cs) {
+	config.gutterSize = config.gutterSize || 0;
+	config.minColumnWidth = config.minColumnWidth || 100;
+	return padding(config.outerGutter ? config.gutterSize : 0, div.all([
+		children(cs.map(function (c) {
+			return c.all([
+				$css('transition', 'top 0.5s, left 0.5s, width 0.5s'),
+			]);
+		})),
+		wireChildren(function (instance, context, is) {
+			var gridCellCount = context.width.map(function (width) {
+				return Math.floor(width / config.minColumnWidth / 2) * 2;
+			});
 
-				var minWidths = Stream.combine(is.map(function (i) {
-					return i.minWidth;
-				}), function () {
-					return Array.prototype.slice.call(arguments);
-				});
-				var minHeights = Stream.combine(is.map(function (i) {
-					return i.minHeight;
-				}), function () {
-					return Array.prototype.slice.call(arguments);
-				});
+			var minWidths = Stream.combine(is.map(function (i) {
+				return i.minWidth;
+			}), function () {
+				return Array.prototype.slice.call(arguments);
+			});
+			var minHeights = Stream.combine(is.map(function (i) {
+				return i.minHeight;
+			}), function () {
+				return Array.prototype.slice.call(arguments);
+			});
 
-				var contexts = [];
-				for (var i = 0; i < is.length; i++) {
-					contexts[i] = {
-						top: Stream.never(),
-						left: Stream.never(),
-						width: Stream.never(),
-						height: Stream.never(),
-					};
-				}
+			var contexts = [];
+			for (var i = 0; i < is.length; i++) {
+				contexts[i] = {
+					top: Stream.never(),
+					left: Stream.never(),
+					width: Stream.never(),
+					height: Stream.never(),
+				};
+			}
 
-				Stream.combine([gridCellCount, context.width, minWidths, minHeights], function (cellCount, width, mws, mhs) {
-					var cellWidth = (width - config.gutterSize * (cellCount - 1)) / cellCount;
-					
-					var currentTop = 0;
-					var colsUsed = 0;
-					var maxHeight = 0;
-					var thisRow = [];
-					
-					var nextRow = function (lastRow) {
-						var colsTaken = 0;
-						var surplusCols = cellCount - colsUsed;
-						var thisRowCellWidth = cellWidth;
-						var extraLeft = 0;
+			Stream.combine([gridCellCount, context.width, minWidths, minHeights], function (cellCount, width, mws, mhs) {
+				var cellWidth = (width - config.gutterSize * (cellCount - 1)) / cellCount;
+				
+				var currentTop = 0;
+				var colsUsed = 0;
+				var maxHeight = 0;
+				var thisRow = [];
+				
+				var nextRow = function (lastRow) {
+					var colsTaken = 0;
+					var surplusCols = cellCount - colsUsed;
+					var thisRowCellWidth = cellWidth;
+					var extraLeft = 0;
 
-						if (lastRow) {
-							var surplusWidth = surplusCols * (cellWidth + config.gutterSize);
-							extraLeft = surplusWidth / 2;
-						}
-						else if (config.splitSurplus) {
-							thisRowCellWidth = (width - config.gutterSize * (colsUsed - 1)) / colsUsed;
-						}
-						else {
-							thisRow[0].cols += surplusCols;
-						}
-						
-						thisRow.map(function (item) {
-							item.context.top.push(currentTop);
-							item.context.left.push(extraLeft + colsTaken * (thisRowCellWidth + config.gutterSize));
-							item.context.width.push(thisRowCellWidth * item.cols + config.gutterSize * (item.cols - 1));
-							item.context.height.push(maxHeight);
-							
-							colsTaken += item.cols;
-						});
-						
-						currentTop += maxHeight + config.gutterSize;
-						colsUsed = 0;
-						maxHeight = 0;
-						thisRow = [];
-					};
-					var pushOntoRow = function (item) {
-						if (colsUsed + item.cols > cellCount) {
-							nextRow();
-						}
-
-						colsUsed += item.cols;
-						maxHeight = Math.max(maxHeight, item.mh);
-						thisRow.push(item);
-					};
-
-					for (var j = 0; j < is.length; j++) {
-						var i = is[j];
-						var mw = mws[j];
-						var mh = mhs[j];
-						var colsTaken = 1;
-						var widthAvailable = cellWidth;
-						while (widthAvailable < mw) {
-							colsTaken += 1;
-							widthAvailable += cellWidth + config.gutterSize;
-						}
-						pushOntoRow({
-							cols: colsTaken,
-							context: contexts[j],
-							mh: mh,
-						});
+					if (lastRow) {
+						var surplusWidth = surplusCols * (cellWidth + config.gutterSize);
+						extraLeft = surplusWidth / 2;
+					}
+					else if (config.splitSurplus) {
+						thisRowCellWidth = (width - config.gutterSize * (colsUsed - 1)) / colsUsed;
+					}
+					else {
+						thisRow[0].cols += surplusCols;
 					}
 					
-					nextRow(true);
-					instance.minHeight.push(currentTop);
-				});
+					thisRow.map(function (item) {
+						item.context.top.push(currentTop);
+						item.context.left.push(extraLeft + colsTaken * (thisRowCellWidth + config.gutterSize));
+						item.context.width.push(thisRowCellWidth * item.cols + config.gutterSize * (item.cols - 1));
+						item.context.height.push(maxHeight);
+						
+						colsTaken += item.cols;
+					});
+					
+					currentTop += maxHeight + config.gutterSize;
+					colsUsed = 0;
+					maxHeight = 0;
+					thisRow = [];
+				};
+				var pushOntoRow = function (item) {
+					if (colsUsed + item.cols > cellCount) {
+						nextRow();
+					}
 
-				return [contexts];
-			}),
-		]));
-	});
+					colsUsed += item.cols;
+					maxHeight = Math.max(maxHeight, item.mh);
+					thisRow.push(item);
+				};
+
+				for (var j = 0; j < is.length; j++) {
+					var i = is[j];
+					var mw = mws[j];
+					var mh = mhs[j];
+					var colsTaken = 1;
+					var widthAvailable = cellWidth;
+					while (widthAvailable < mw) {
+						colsTaken += 1;
+						widthAvailable += cellWidth + config.gutterSize;
+					}
+					pushOntoRow({
+						cols: colsTaken,
+						context: contexts[j],
+						mh: mh,
+					});
+				}
+				
+				nextRow(true);
+				instance.minHeight.push(currentTop);
+			});
+
+			return [contexts];
+		}),
+	]));
+};
 
 var backgroundImagePosition = {
 	fit: 'fit',
