hcj-js/hcj

commit 7ab6b5ade0614bdf4911d796d435c52119520a10

rootComponent: give component full window height
Even if its min height is less than the window height.

dist/hcj.js
@@ -979,23 +979,25 @@ function waitForWebfonts(fonts, callback, maxTime) {
     ], function (ww, wh, mh) {
       var mhAtWW = mh(ww);
       var mhAtScrollbarWW = mh(ww - scrollbarWidth);
+      var componentMinWidth;
+      var componentMinHeight;
       if (mhAtWW > wh) {
+        componentMinWidth = ww - scrollbarWidth;
+        componentMinHeight = mhAtScrollbarWW;
         if (mhAtScrollbarWW > wh) {
           document.body.style.overflowY = 'initial';
-          stream.push(width, ww - scrollbarWidth);
-          stream.push(height, mhAtScrollbarWW);
         }
         else {
           document.body.style.overflowY = 'scroll';
-          stream.push(width, ww - scrollbarWidth);
-          stream.push(height, mhAtScrollbarWW);
         }
       }
       else {
+        componentMinWidth = ww;
+        componentMinHeight = mhAtWW;
         document.body.style.overflowY = 'initial';
-        stream.push(width, ww);
-        stream.push(height, mhAtWW);
       }
+      stream.push(width, Math.max(ww, componentMinWidth));
+      stream.push(height, Math.max(wh, componentMinHeight));
     });
     var i = rootLayout(c)({
       el: document.body,

docs/hcj.js
@@ -979,23 +979,25 @@ function waitForWebfonts(fonts, callback, maxTime) {
     ], function (ww, wh, mh) {
       var mhAtWW = mh(ww);
       var mhAtScrollbarWW = mh(ww - scrollbarWidth);
+      var componentMinWidth;
+      var componentMinHeight;
       if (mhAtWW > wh) {
+        componentMinWidth = ww - scrollbarWidth;
+        componentMinHeight = mhAtScrollbarWW;
         if (mhAtScrollbarWW > wh) {
           document.body.style.overflowY = 'initial';
-          stream.push(width, ww - scrollbarWidth);
-          stream.push(height, mhAtScrollbarWW);
         }
         else {
           document.body.style.overflowY = 'scroll';
-          stream.push(width, ww - scrollbarWidth);
-          stream.push(height, mhAtScrollbarWW);
         }
       }
       else {
+        componentMinWidth = ww;
+        componentMinHeight = mhAtWW;
         document.body.style.overflowY = 'initial';
-        stream.push(width, ww);
-        stream.push(height, mhAtWW);
       }
+      stream.push(width, Math.max(ww, componentMinWidth));
+      stream.push(height, Math.max(wh, componentMinHeight));
     });
     var i = rootLayout(c)({
       el: document.body,

src/hcj.js
@@ -901,23 +901,25 @@
     ], function (ww, wh, mh) {
       var mhAtWW = mh(ww);
       var mhAtScrollbarWW = mh(ww - scrollbarWidth);
+      var componentMinWidth;
+      var componentMinHeight;
       if (mhAtWW > wh) {
+        componentMinWidth = ww - scrollbarWidth;
+        componentMinHeight = mhAtScrollbarWW;
         if (mhAtScrollbarWW > wh) {
           document.body.style.overflowY = 'initial';
-          stream.push(width, ww - scrollbarWidth);
-          stream.push(height, mhAtScrollbarWW);
         }
         else {
           document.body.style.overflowY = 'scroll';
-          stream.push(width, ww - scrollbarWidth);
-          stream.push(height, mhAtScrollbarWW);
         }
       }
       else {
+        componentMinWidth = ww;
+        componentMinHeight = mhAtWW;
         document.body.style.overflowY = 'initial';
-        stream.push(width, ww);
-        stream.push(height, mhAtWW);
       }
+      stream.push(width, Math.max(ww, componentMinWidth));
+      stream.push(height, Math.max(wh, componentMinHeight));
     });
     var i = rootLayout(c)({
       el: document.body,

commit bde8bf189a421584d308743753f95f5c7d26686a

hcj: fix window width bug introduced in 7ab6b5a
Root component would be assigned the full window width, even if there
was a vertical scrollbar.

dist/hcj.js
@@ -996,7 +996,7 @@ function waitForWebfonts(fonts, callback, maxTime) {
         componentMinHeight = mhAtWW;
         document.body.style.overflowY = 'initial';
       }
-      stream.push(width, Math.max(ww, componentMinWidth));
+      stream.push(width, componentMinWidth);
       stream.push(height, Math.max(wh, componentMinHeight));
     });
     var i = rootLayout(c)({

docs/hcj.js
@@ -996,7 +996,7 @@ function waitForWebfonts(fonts, callback, maxTime) {
         componentMinHeight = mhAtWW;
         document.body.style.overflowY = 'initial';
       }
-      stream.push(width, Math.max(ww, componentMinWidth));
+      stream.push(width, componentMinWidth);
       stream.push(height, Math.max(wh, componentMinHeight));
     });
     var i = rootLayout(c)({

src/hcj.js
@@ -918,7 +918,7 @@
         componentMinHeight = mhAtWW;
         document.body.style.overflowY = 'initial';
       }
-      stream.push(width, Math.max(ww, componentMinWidth));
+      stream.push(width, componentMinWidth);
       stream.push(height, Math.max(wh, componentMinHeight));
     });
     var i = rootLayout(c)({