hcj-js/hcj

commit ee4c8423b58d95ef697e881fcba598fc8ada1cb8

instead of noRemove, use a config object

dist/hcj.js
@@ -544,13 +544,13 @@ function waitForWebfonts(fonts, callback, maxTime) {
   });
   var windowWidthMinusScrollbar = stream.create();
 
-  var windowScroll = stream.create(true);
+  var windowScroll = stream.create();
   window.addEventListener('scroll', function () {
     stream.push(windowScroll, window.scrollY);
   });
   stream.push(windowScroll, window.scrollY);
 
-  var windowHash = stream.create(true);
+  var windowHash = stream.create();
   window.addEventListener('hashchange', function () {
     stream.push(windowHash, location.pathname);
   });
@@ -866,7 +866,7 @@ function waitForWebfonts(fonts, callback, maxTime) {
       return "calc(" + x + ")";
     });
   };
-  var layoutAppend = function (childInstances, el, context, c, ctx, noRemove) {
+  var layoutAppend = function (childInstances, el, context, c, ctx, config) {
     ctx = ctx || {};
     var childWidth = stream.create();
     var childWidthCalc = stream.create();
@@ -987,7 +987,7 @@ function waitForWebfonts(fonts, callback, maxTime) {
         unpushContextLeft();
       },
     });
-    if (noRemove !== true) {
+    if (config && config.noRemove !== true) {
       childInstances.push(i);
     }
     // todo: replace with some isInstance function
@@ -1020,8 +1020,8 @@ function waitForWebfonts(fonts, callback, maxTime) {
       console.log('cs is not a function');
       debugger;
     }
-    return function (ctx, noRemove) {
-      return layoutAppend(childInstances, el, context, cs, ctx, noRemove);
+    return function (ctx, config) {
+      return layoutAppend(childInstances, el, context, cs, ctx, config);
     };
   };
 
@@ -1056,8 +1056,8 @@ function waitForWebfonts(fonts, callback, maxTime) {
     return component(el, function (el, context) {
       var childInstances = [];
       el.style.position = 'absolute';
-      var i = buildContainer(el, context, function (c, ctx, noRemove) {
-        return layoutAppend(childInstances, el, context, c, ctx, noRemove);
+      var i = buildContainer(el, context, function (c, ctx, config) {
+        return layoutAppend(childInstances, el, context, c, ctx, config);
       });
       return {
         el: el,
@@ -3486,7 +3486,9 @@ function waitForWebfonts(fonts, callback, maxTime) {
         i = append(c, {
           widthCalc: stream.once('100%'),
           heightCalc: stream.once('100%'),
-        }, true);
+        }, {
+          noRemove: true,
+        });
         unpushMW = stream.pushAll(i.minWidth, minWidth);
         unpushMH = stream.pushAll(i.minHeight, minHeight);
         return i;
@@ -3533,7 +3535,9 @@ function waitForWebfonts(fonts, callback, maxTime) {
           widthCalc: stream.once('100%'),
           heightCalc: stream.once('100%'),
         };
-        i = append(c, ctx, true);
+        i = append(c, ctx, {
+          noRemove: true,
+        });
         i.el.style.transition = 'inherit';
         i.el.style.display = 'none';
         el.prepend(i.el);

docs/hcj.js
@@ -544,13 +544,13 @@ function waitForWebfonts(fonts, callback, maxTime) {
   });
   var windowWidthMinusScrollbar = stream.create();
 
-  var windowScroll = stream.create(true);
+  var windowScroll = stream.create();
   window.addEventListener('scroll', function () {
     stream.push(windowScroll, window.scrollY);
   });
   stream.push(windowScroll, window.scrollY);
 
-  var windowHash = stream.create(true);
+  var windowHash = stream.create();
   window.addEventListener('hashchange', function () {
     stream.push(windowHash, location.pathname);
   });
@@ -866,7 +866,7 @@ function waitForWebfonts(fonts, callback, maxTime) {
       return "calc(" + x + ")";
     });
   };
-  var layoutAppend = function (childInstances, el, context, c, ctx, noRemove) {
+  var layoutAppend = function (childInstances, el, context, c, ctx, config) {
     ctx = ctx || {};
     var childWidth = stream.create();
     var childWidthCalc = stream.create();
@@ -987,7 +987,7 @@ function waitForWebfonts(fonts, callback, maxTime) {
         unpushContextLeft();
       },
     });
-    if (noRemove !== true) {
+    if (config && config.noRemove !== true) {
       childInstances.push(i);
     }
     // todo: replace with some isInstance function
@@ -1020,8 +1020,8 @@ function waitForWebfonts(fonts, callback, maxTime) {
       console.log('cs is not a function');
       debugger;
     }
-    return function (ctx, noRemove) {
-      return layoutAppend(childInstances, el, context, cs, ctx, noRemove);
+    return function (ctx, config) {
+      return layoutAppend(childInstances, el, context, cs, ctx, config);
     };
   };
 
@@ -1056,8 +1056,8 @@ function waitForWebfonts(fonts, callback, maxTime) {
     return component(el, function (el, context) {
       var childInstances = [];
       el.style.position = 'absolute';
-      var i = buildContainer(el, context, function (c, ctx, noRemove) {
-        return layoutAppend(childInstances, el, context, c, ctx, noRemove);
+      var i = buildContainer(el, context, function (c, ctx, config) {
+        return layoutAppend(childInstances, el, context, c, ctx, config);
       });
       return {
         el: el,
@@ -3486,7 +3486,9 @@ function waitForWebfonts(fonts, callback, maxTime) {
         i = append(c, {
           widthCalc: stream.once('100%'),
           heightCalc: stream.once('100%'),
-        }, true);
+        }, {
+          noRemove: true,
+        });
         unpushMW = stream.pushAll(i.minWidth, minWidth);
         unpushMH = stream.pushAll(i.minHeight, minHeight);
         return i;
@@ -3533,7 +3535,9 @@ function waitForWebfonts(fonts, callback, maxTime) {
           widthCalc: stream.once('100%'),
           heightCalc: stream.once('100%'),
         };
-        i = append(c, ctx, true);
+        i = append(c, ctx, {
+          noRemove: true,
+        });
         i.el.style.transition = 'inherit';
         i.el.style.display = 'none';
         el.prepend(i.el);

src/hcj.js
@@ -789,7 +789,7 @@
       return "calc(" + x + ")";
     });
   };
-  var layoutAppend = function (childInstances, el, context, c, ctx, noRemove) {
+  var layoutAppend = function (childInstances, el, context, c, ctx, config) {
     ctx = ctx || {};
     var childWidth = stream.create();
     var childWidthCalc = stream.create();
@@ -910,7 +910,7 @@
         unpushContextLeft();
       },
     });
-    if (noRemove !== true) {
+    if (config && config.noRemove !== true) {
       childInstances.push(i);
     }
     // todo: replace with some isInstance function
@@ -943,8 +943,8 @@
       console.log('cs is not a function');
       debugger;
     }
-    return function (ctx, noRemove) {
-      return layoutAppend(childInstances, el, context, cs, ctx, noRemove);
+    return function (ctx, config) {
+      return layoutAppend(childInstances, el, context, cs, ctx, config);
     };
   };
 
@@ -979,8 +979,8 @@
     return component(el, function (el, context) {
       var childInstances = [];
       el.style.position = 'absolute';
-      var i = buildContainer(el, context, function (c, ctx, noRemove) {
-        return layoutAppend(childInstances, el, context, c, ctx, noRemove);
+      var i = buildContainer(el, context, function (c, ctx, config) {
+        return layoutAppend(childInstances, el, context, c, ctx, config);
       });
       return {
         el: el,
@@ -3409,7 +3409,9 @@
         i = append(c, {
           widthCalc: stream.once('100%'),
           heightCalc: stream.once('100%'),
-        }, true);
+        }, {
+          noRemove: true,
+        });
         unpushMW = stream.pushAll(i.minWidth, minWidth);
         unpushMH = stream.pushAll(i.minHeight, minHeight);
         return i;
@@ -3456,7 +3458,9 @@
           widthCalc: stream.once('100%'),
           heightCalc: stream.once('100%'),
         };
-        i = append(c, ctx, true);
+        i = append(c, ctx, {
+          noRemove: true,
+        });
         i.el.style.transition = 'inherit';
         i.el.style.display = 'none';
         el.prepend(i.el);

docs/docs.js
@@ -514,10 +514,23 @@
       p("The `hcj.component.container` method is a helper method for defining layouts.  It takes an optional string argument giving tag name of the container, followed by a build method."),
       p("`container :: (String? , BuildContainer) -> Component`"),
       p("`type BuildContainer = (JQuery, Context, Append) -> {minWidth :: Number, minHeight :: Stream (Number -> Number), remove :: Function?}`"),
-      p("`type Append = (Component, Viewport?, Bool?) -> Instance`"),
+      p("`type Append = (Component, Viewport?, ChildConfig?) -> Instance`"),
+      c.stack([
+        p("`type ChildConfig = {`"),
+        c.all([
+          c.margin({
+            left: 20,
+          }),
+        ])(objectDefinition([{
+          name: 'noRemove',
+          type: 'Bool?',
+          description: 'Indicates that child component will be removed manually.',
+        }], true)),
+        p("`}`"),
+      ]),
       p("The build method takes three arguments.  The first two, `el` and `context`, are the root element of the container and the context that it is rendered into.  The third argument, `append`, is used to append child components to the container."),
-      p("The `append` function takes three arguments: the `Component` to append, a `Viewport`, and a `noRemove` flag."),
-      p("The first argument is self-explanatory.  The second argument, `viewport`, is an object that is upgraded to a `context` and then passed into the component being appended.  The `noRemove` flag causes the `remove` function of the returned `instance` not to be run when the container itself is removed.  This should be set to true if you intend the remove the instance yourself, typically at an earlier time than when the container is removed."),
+      p("The `append` function takes three arguments: the `Component` to append, a `Viewport`, and a `ChildConfig` object."),
+      p("The first argument is self-explanatory.  The second argument, `viewport`, is an object that is upgraded to a `context` and then passed into the component being appended.  If the `noRemove` property of the `ChildConfig` object is `true`, this means that you intend to call the `remove` function of the appended child component manually, so it will not be called automatically when the container is removed from the DOM."),
       p('A `Viewport` is an object with the following properties:'),
       objectDefinition([{
         name: 'el',

commit 589a46c812eb4fd45ef5cbf2c9d5b44f189988fa

layout: fix bad bug introduced in ee4c842

dist/hcj.js
@@ -987,7 +987,7 @@ function waitForWebfonts(fonts, callback, maxTime) {
         unpushContextLeft();
       },
     });
-    if (config && config.noRemove !== true) {
+    if (!config || config.noRemove !== true) {
       childInstances.push(i);
     }
     // todo: replace with some isInstance function

docs/hcj.js
@@ -987,7 +987,7 @@ function waitForWebfonts(fonts, callback, maxTime) {
         unpushContextLeft();
       },
     });
-    if (config && config.noRemove !== true) {
+    if (!config || config.noRemove !== true) {
       childInstances.push(i);
     }
     // todo: replace with some isInstance function

src/hcj.js
@@ -910,7 +910,7 @@
         unpushContextLeft();
       },
     });
-    if (config && config.noRemove !== true) {
+    if (!config || config.noRemove !== true) {
       childInstances.push(i);
     }
     // todo: replace with some isInstance function